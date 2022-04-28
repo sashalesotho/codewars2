3babaac25c97b59f833801e19fe433be42260e20
sash <na-okne@mail.ru>
2022-04-28 14:22:19 +0300

    first commit

---
+++ nuclear missile manager.js
@@ -0,0 +1,13 @@
+/*
+Our nuclear missile manager system is coded in Node.js. We are currently testing the system and it seems that the launchAll function does not work as expected. It should launch 5 missiles each 1 second apart. The current code tries to launch the missile #5 five times...
+
+Can you fix this for us? You know, it's pretty critical code...
+
+Note: There are 5 missiles labeled i which is a number in {0, 1, 2, 3, 4}. The missile i should be launched after i seconds.
+*/
+
+function launchAll(launchMissile) {
+	for (let i = 0; i < 5; i++) {
+	  setTimeout(() => launchMissile(i), i * 1000);
+	}
+ }
